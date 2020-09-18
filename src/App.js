30b6302453d2608e724d8b21267c4a158a49515e
Your_Name <joseph161216@gmail.com>
2020-09-17 22:52:32 -0700

    completed search and render data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import data from "./db.json"
 
 function App() {
+  // i'm initializing data.post from db.json file as filteredList
+  const [filteredList, setFilteredList] = useState(data.posts)
+  const [searchWord, setSearchWord] = useState("")
+  // console.log(data)
+
+  const handleChange = (e) => {
+    setSearchWord(e.target.value)
+
+    if (e.target.value !== "") {
+      setFilteredList(
+        data.posts.filter((post) =>
+          post.title.toLowerCase().includes(e.target.value)
+        )
+      );
+    } else {
+      setFilteredList(data.posts);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>Search through the Data</label>
+      <input placeholder="type something to search" value={searchWord} onChange={handleChange}></input>
+      <ul>
+      {
+        filteredList.map((post, index) =>
+          <li key={index}>{post.title}</li>
+        )
+      }
+      </ul>
     </div>
   );
 }
